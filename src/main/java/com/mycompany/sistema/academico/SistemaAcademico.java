e8235bb79dbd4764071bbcf9efd8b5ba6e4a4bb2
Carolina Valladolid <carolinavalladolidbravo@gmail.com>
2025-06-17 21:53:17 -0500

    Main

---
+++ src/main/java/com/mycompany/sistema/academico/SistemaAcademico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema.academico;
+
+/**
+ *
+ * @author HP
+ */
+public class SistemaAcademico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
