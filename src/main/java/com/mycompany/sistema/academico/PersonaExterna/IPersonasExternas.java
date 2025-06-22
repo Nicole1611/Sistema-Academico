2a1f39d33ebd86f37ab050fcd2869564a14ab314
jojanci <jojancigomez@gmail.com>
2025-06-22 16:57:39 -0500

    sistema academico

---
+++ src/main/java/com/mycompany/sistema/academico/PersonaExterna/IPersonasExternas.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.sistema.academico.PersonaExterna;
+
+
+public interface IPersonasExternas {
+    public boolean verificarCedula();
+    public String agregarPersonaExterna();
+}
